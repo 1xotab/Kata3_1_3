e04061c09d4658b299cb116db7ec31987eec6de8
1xotab <ninetonine319@gmail.com>
2022-04-26 22:20:12 +0300

    Rest refactoring

--- src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/AdminController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +24,6 @@ import java.util.Set;
 
 
 @Controller
-@PreAuthorize("hasAuthority('ADMIN')")
 public class AdminController {
 
     @Autowired
